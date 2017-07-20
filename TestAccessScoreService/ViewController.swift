8f51f94a827dd5ee1400bae2e7ba298c5cecb69f
Tom Patterson <tom.patterson@comcast.com>
2017-07-20 10:42:38 -0700

    Initial Commit

---
+++ TestAccessScoreService/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAccessScoreService
+//
+//  Created by Tom Patterson on 7/20/17.
+//  Copyright © 2017 Tom Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
